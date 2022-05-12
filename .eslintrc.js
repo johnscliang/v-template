9c03a436218a15e5929f217c94e90a8873cc0fe5
liangshicong <809556311@qq.com>
2022-05-13 01:19:36 +0800

    commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'semi': [2, 'never'],
+    'vue/no-multiple-template-root': 'off',
+    'comma-dangle': 'off',
+    'no-undef': 'off'
+  },
+};
