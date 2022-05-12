318253282190e8af72a9a88a87dec863a056cdf1
liangshicong <809556311@qq.com>
2022-05-13 01:23:43 +0800

    chore: rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'type-empty': [2, 'never'],
         'type-enum': [
             2, 'always', [
-                '+',
+                'feat',
                 'fix',
                 'style',
                 'docs',
