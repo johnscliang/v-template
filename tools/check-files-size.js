4ef9f48ed5450f6a9eea40e96fa9c60b5a6a2c3a
liangshicong <809556311@qq.com>
2022-05-13 18:47:34 +0800

    chore: tools

---
+++ tools/check-files-size.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const filesize = require("filesize");
+const files = process.argv.slice(2);
+
+const SIZE_LIMIT = 5 * 1024;
+const filesOverLimit = [];
+
+for (const file of files) {
+  const { size } = fs.statSync(file);
+  if (size > SIZE_LIMIT) {
+    filesOverLimit.push({ name: file, size });
+  }
+}
+
+if (filesOverLimit.length > 0) {
+  console.error(
+    `The following files are over the size limit (${filesize(SIZE_LIMIT)}):`
+  );
+  filesOverLimit.forEach(file =>
+    console.error(`\t* ${filesize(file.size)} ${file.name}`)
+  );
+  console.error("Please optimize the assets using https://imageoptim.com/mac");
+
+  process.exit(1);
+}
