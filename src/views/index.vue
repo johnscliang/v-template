03be8b80460dcd744336642360af250902d950c0
liangshicong <809556311@qq.com>
2022-05-13 22:41:11 +0800

    chore: cscc

---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test-container page-container">
+    aaaa
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainIndex',
+  setup() {
+    const count = ref<number>(0)
+    const increment = () => {
+      count.value += 1
+    }
+    return { count, increment }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.aa{}
+</style>
