5f5998065ae64e16e2bd561d70e5104fd86fa8cc
liangshicong <809556311@qq.com>
2022-05-28 01:14:49 +0800

    chore: test

--- src/common/components/idv/IdCardOCR.vue
+++ src/common/components/idv/IdCardOCR.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        拍照
+        <video id="video" width="680px" height="400px" autoplay/>
     </div>
 </template>
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
 
-}
+export default defineComponent({
+  name: 'IdCardOCR',
+  setup() {
+    const count = ref<number>(0)
+    const increment = () => {
+      count.value += 1
+    }
+    return { count, increment }
+  },
+  methods: {
+    init() {
+      const promise = navigator.mediaDevices.getUserMedia({
+        video: {
+          width: 400,
+          height: 680
+        }
+      })
+      const video = document.querySelector('#video')
+      promise.then(function (stream) {
+        // video.obj
+      })
+    }
+  },
+  onMounted() {
+    this.init()
+  }
+})
 </script>
 <style scoped lang="scss">
 
