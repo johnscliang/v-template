be193af266995aa604c8f797e70958d6c19c46be
liangshicong <809556311@qq.com>
2022-06-02 14:18:19 +0800

    chore: add vant dynamic

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src') // `@` --> `src`
